89f0c97540e043dc07451f000c7ae6a2dd5e767a
ArasElyte <aras@elitelyfe.com>
2021-01-27 15:48:50 -0800

    Initial Load - Key Error in config

--- app/email.py
+++ app/email.py
@@ -7,10 +7,11 @@ def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
-def send_email(subject, body):
-    msg = Message(subject, sender='aras@condatus.com', recipients=['arasged@gmail.com'])
-    msg.body = body
-    msg.html = 'This is <b>HTML</b>'
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(subject, sender='aras@condatus.com', recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app,msg])
     thr.start()
     return thr
